102c85bf3267ff5534ba90b835f81673275d8f41
Nicholas Leacock <nicholas@swissmail.com>
2017-02-20 17:16:11 +0000

    First tests + Oystercard implementation

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,19 @@
+require 'oystercard'
+
 describe Oystercard do
+  subject(:card) {described_class.new}
+
+  it "responds to top_up" do
+    expect(card).to respond_to(:top_up)
+  end
+
+  it "has a balance" do
+    expect(card.balance).not_to be nil
+  end
+
+  it "has a default balance" do
+    expect(card.balance).to eq 0
+  end
+
 
 end
